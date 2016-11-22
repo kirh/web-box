d34cd7a6ad9321469262924634f1646dc1ad82d7
kirh <hilman91@gmail.com>
2016-11-22 21:29:16 +0300

    fix

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
-def wsgi_aplication(environ, start_response):
+def my_app(environ, start_response):
 	status = '200 OK'
 	headers = [
 		('Content_Type', 'text/plain')
 		]
-	body = 'Hello World'
+	body = ""	
+	for line in environ['QUERY_STRING'].split('&'):
+		body = body + line + "\n"
+    #for line in environ["QUERY_STRING"].split('&')
+	#	body = body+line+"\n"	
 	start_response(status, headers)
-	return [ body ]	
+	return body
