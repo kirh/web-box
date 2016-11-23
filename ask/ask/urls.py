b545b8f7c591a800ce20048d2a02ab37539f2315
kirh <hilman91@gmail.com>
2016-11-23 03:47:35 +0300

    fix

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 #from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', 'qa.views.test'),
+    url(r'^$', 'qa.views.test'),
     url(r'^login/', 'qa.views.test', name='login'),
     url(r'^signup/', 'qa.views.test', name='signup'),
-    url(r'^question/?P<question>[0-9])/', 'qa.views.test', name='question'),
+    url(r'^question/\d+/$', 'qa.views.test', name='question'),
     url(r'^ask/', 'qa.views.test', name='ask'),
     url(r'^popular/', 'qa.views.test', name='popular'),
     url(r'^new/', 'qa.views.test', name='new'),
+    url(r'^/', 'qa.views.test'),
 ]
