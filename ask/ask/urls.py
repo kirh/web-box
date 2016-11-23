22c76cb2825252e07a0fbc2aa42b7c045f360356
kirh <hilman91@gmail.com>
2016-11-23 03:57:21 +0300

    fix

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 #from django.conf.urls import include, url
 #from django.contrib import admin
-
+"""
 urlpatterns = [
     url(r'^$', 'qa.views.test'),
     url(r'^login/', 'qa.views.test', name='login'),
@@ -26,3 +26,18 @@ urlpatterns = [
     url(r'^new/', 'qa.views.test', name='new'),
     url(r'^/', 'qa.views.test'),
 ]
+"""
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', 'qa.views.test', name='home'),
+    url(r'^login/.*$', 'qa.views.test', name='login'),
+    url(r'^signup/.*$', 'qa.views.test', name='signup'),
+    url(r'^question/\d+/$', 'qa.views.test', name='question'),
+    url(r'^ask/.*$', 'qa.views.test', name='ask'),
+    url(r'^popular/.*$', 'qa.views.test', name='popular'),
+    url(r'^new/.*$', 'qa.views.test', name='new')
+)
