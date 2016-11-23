79b0e5502f59761f97390869f8ec716b11c4b920
kirh <hilman91@gmail.com>
2016-11-23 08:47:19 +0300

    fix vers

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse 
 def test(request, *args, **kwargs):
-	return HttpResponse('OK')
+    return HttpResponse('OK')
