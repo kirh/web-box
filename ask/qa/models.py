57b2c45155aef1f3f3707f2d3ab780212ad4b9a3
kirh <hilman91@gmail.com>
2016-11-24 06:24:28 +0300

    add models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+Class QuestionManager(models.Manager):
+	def new(self):
+		return self.objects.all()[:10]
+
+	def popular(self):
+		return self.objects.order_by('rating').all()
+		
+Class Question(models.Model):
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateTimeField()
+	rating = models.IntegerField(default=0)
+	author = models.ForeignKey(User)
+	likes = models.ManyToManyField(User)
+	objects = QuestionManager
+	Class Meta:
+		ordering = ['-added_at']
+
+Class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField()
+	question = models.OneToOneField(Question)
+	author = models.ForeignKey(User)
+	Class Meta:
+		ordering = ['-added_at']
--- init.sh
+++ init.sh
@@ -4,3 +4,7 @@ sudo /etc/init.d/nginx restart
 #gunicorn
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
+#mysql
+sudo /etc/init.d/mysql start
+sudo mysql -uroot -e "CREATE DATABASE Ask"
+sudo /home/box/web/ask/manage.py syncdb
