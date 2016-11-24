5acf19a335ee7918b60e7a2dd7f060d907220b7c
kirh <hilman91@gmail.com>
2016-11-24 08:15:26 +0300

    x3

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -13,17 +13,17 @@ class QuestionManager(models.Manager):
 class Question(models.Model):
 	title = models.CharField(max_length=255)
 	text = models.TextField()
-	added_at = models.DateTimeField()
+	added_at = models.DateTimeField(auto_now=True)
 	rating = models.IntegerField()
 	author = models.ForeignKey(User)
-	likes = models.ManyToManyField(User,related_name='question_like_user')
+	likes = models.ManyToManyField(User)
 	objects = QuestionManager()
 	class Meta:
 		ordering = ['-added_at']
 
 class Answer(models.Model):
 	text = models.TextField()
-	added_at = models.DateTimeField()
+	added_at = models.DateTimeField(auto_now=True)
 	question = models.OneToOneField(Question)
 	author = models.ForeignKey(User)
 	class Meta:
--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
 #mysql
 sudo /etc/init.d/mysql start
-sudo mysql -uroot -e "CREATE DATABASE ask"
-mysql -uroot -e "CREATE USER 'anyuser'@'localhost' IDENTIFIED BY 'any'"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON ASK.* TO 'any'@'localhost'"
+sudo mysql -uroot -e "CREATE DATABASE askdb"
+#mysql -uroot -e "CREATE USER 'anyuser'@'localhost' IDENTIFIED BY 'any'"
+#mysql -uroot -e "GRANT ALL PRIVILEGES ON ASK.* TO 'any'@'localhost'"
 sudo /home/box/web/ask/manage.py syncdb
