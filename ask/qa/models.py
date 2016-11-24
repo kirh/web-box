64ba6415b2769bdaca349a6739663f4634d05442
kirh <hilman91@gmail.com>
2016-11-24 08:30:44 +0300

    x3

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -13,8 +13,8 @@ class QuestionManager(models.Manager):
 class Question(models.Model):
 	title = models.CharField(max_length=255)
 	text = models.TextField()
-	added_at = models.DateTimeField(auto_now=True)
-	rating = models.IntegerField()
+	added_at = models.DateTimeField(blank=True,auto_now_add=True)
+	rating = models.IntegerField(default=0)
 	author = models.ForeignKey(User)
 	likes = models.ManyToManyField(User,related_name='question_like_user')
 	objects = QuestionManager()
@@ -23,7 +23,7 @@ class Question(models.Model):
 
 class Answer(models.Model):
 	text = models.TextField()
-	added_at = models.DateTimeField(auto_now=True)
+	added_at = models.DateTimeField(blank=True,auto_now_add=True)
 	question = models.OneToOneField(Question)
 	author = models.ForeignKey(User)
 	class Meta:
